7d74e14a6ae74434d51598f45f3c525185021e4b
Rakhshanda Ali <69416106+rakii8458@users.noreply.github.com>
2023-06-06 23:47:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
 
-import React, {useState, useEffect} from 'react'
-import Theme from './Theme';
-import {ThemeContext, themes} from './Context/contextdark';
+import React from 'react'
+// import Todos from './Todos'
+import Todo1 from './Todo1';
 
-
-
+import './App.css';
 
 function App() {
-const [mytheme , setTheme] = useState(themes.dark);
-
-const body= document.body;
-useEffect(()=>{
-  switch(mytheme){
-    case themes.light:
-      body.classList.remove("bg-dark");
-      body.classList.remove("text-light");
-      body.classList.add("bg-light");
-      body.classList.add("text-dark");
-      break
-    case themes.dark:
-      body.classList.remove("bg-light");
-      body.classList.remove("text-dark");
-      body.classList.add("bg-dark");
-      body.classList.add("text-light");
-      break
-      default:
-        body.classList.remove("bg-dark");
-        body.classList.remove("text-light");
-        body.classList.add("bg-light");
-        body.classList.add("text-dark");
-  }
-
-}, [mytheme])
-
-//btn
-
-function onHandleclick(){
- mytheme === themes.dark ? setTheme(themes.light) :setTheme(themes.dark) ;
- 
-}
   return (
-  <>
-  <ThemeContext.Provider value={{mytheme , onHandleclick}}>
-  <div className="main-container">
-      <h1 className="text-center">Theme Changer Web Page</h1>
-      <Theme theme={mytheme}/>
-    
-      
-    
-   </div>
-
-  </ThemeContext.Provider>
- 
-
- 
-  </>
-
-    
-    
+    <div className="app">
+      {/* <h1>Hello I am a Todos App</h1> */}
+      <Todo1/>
+    </div>
   );
 }
 
